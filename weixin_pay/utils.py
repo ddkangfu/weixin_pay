fbf147d8a39b50d1efa1d534132b8628a013e52b
ddkangfu <ddkangfu@gmail.com>
2014-11-15 23:37:20 +0800

    增加了计算sign和生成xml的工具类函数。

--- weixin_pay/utils.py
+++ weixin_pay/utils.py
@@ -1,5 +1,7 @@
 # -*- coding=utf-8 -*-
 
+import hashlib
+
 def smart_str(s, encoding='utf-8', strings_only=False, errors='strict'):
     """
     Returns a bytestring version of 's', encoded as specified in 'encoding'.
@@ -27,9 +29,26 @@ def smart_str(s, encoding='utf-8', strings_only=False, errors='strict'):
         return s
 
 
-def calculate_md5_code(per_url, auth_key):
-    pass
+def calculate_sign(params, auth_key):
+    #签名步骤一：按字典序排序参数
+    sorted(params)
+    #签名步骤二：在string后加入KEY
+    url = "&".join(['%s=%s'%(k, v) for (k, v) in params.items])
+    url = '%s&key=%s' % (url, auth_key)
+    #签名步骤三：MD5加密, 所有字符转为大写
+    return hashlib.md5(url).upper()
 
 
 def dict_to_xml(params):
-    pass
+    xml = ["<xml>",]
+    for (k, v) in params.items():
+        if (v.isdigit()):
+            xml.append('<%s>%s</%s>' % (k, v, k))
+        else:
+            xml.append('<%s><![DATA[%s]]></%s>' % (k, v, k))
+    xml.append('</xml>')
+    return ''.join(xml)
+
+if __name__ == '__main__':
+    params = {"abc": 'abc123', "123": "123"}
+    print dict_to_xml(params)
