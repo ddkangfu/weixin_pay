a76148ecdcea557d246d04094d0fbb04737663f3
ddkangfu <ddkangfu@gmail.com>
2014-11-16 00:06:45 +0800

    修改了测试用例的测试方式。

--- weixin_pay/tests.py
+++ weixin_pay/tests.py
@@ -2,6 +2,8 @@
 
 import unittest
 
+from utils import dict_to_xml
+
 
 class TestUtils(unittest.TestCase):
     #def test_calculate_sign(self):
@@ -18,8 +20,17 @@ class TestUtils(unittest.TestCase):
         expect_result = "<xml><123><![DATA[xyz123]]></123></xml>"
         self.assertEqual(result, expect_result)
 
-        params = {"abc": "abc", "123": "123"}
+        #params = {"abc": "abc", "123": "123"}
+        params = {"123": "123", "abc": "abc", }
         result = dict_to_xml(params)
         expect_result = "<xml><123>123</123><abc><![DATA[abc]]></abc></xml>"
         self.assertEqual(result, expect_result)
 
+        params = {"c": "123", "a": "abc", }
+        result = dict_to_xml(params)
+        expect_result = "<xml><a><![DATA[abc]]></a><c>123</c></xml>"
+        self.assertEqual(result, expect_result)
+
+
+if __name__ == "__main__":
+    unittest.main()
